6c001a41c652af29ba0b9e82c0da7620b362fec3
youngheeJo <younghee.dev@gmail.com>
2022-04-08 11:06:03 +0900

    Chapter3. Exercise 코드 세팅

---
+++ src/test/java/list/MyLinkedListTest.java
@@ -0,0 +1,19 @@
+package list;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
+public class MyLinkedListTest extends MyArrayListTest {
+    @BeforeEach
+    void setup() {
+        list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        mylist = new MyLinkedList<>();
+        mylist.addAll(list);
+    }
+}
