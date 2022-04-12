c9f121ae7e84413affb71277d41133397e3d23f8
youngheeJo <younghee.dev@gmail.com>
2022-04-12 18:12:41 +0900

    Chapter7. 위키피디아 크롤러 코드 세팅

---
+++ src/test/java/crawling/WikiParserTest.java
@@ -0,0 +1,22 @@
+package crawling;
+
+import org.junit.jupiter.api.Test;
+
+public class WikiParserTest {
+
+    @Test
+    void testIsInParentheses() {
+
+    }
+
+    @Test
+    void testIsLink() {
+
+    }
+
+    @Test
+    void tsetIsItalic() {
+
+    }
+
+}
