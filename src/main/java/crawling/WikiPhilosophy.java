a5f14e2cce6660f5a07c2d168bf056fafe794133
youngheeJo <younghee.dev@gmail.com>
2022-04-15 16:48:42 +0900

    Chapter7. 위키피디아 크롤러 testConjecture 메서드 구현

--- src/main/java/crawling/WikiPhilosophy.java
+++ src/main/java/crawling/WikiPhilosophy.java
@@ -1,5 +1,8 @@
 package crawling;
 
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +29,7 @@ public class WikiPhilosophy {
         String destination = "https://en.wikipedia.org/wiki/Philosophy";
         String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 
-        testConjecture(destination, source, 10);
+        testConjecture(destination, source, 11);
     }
 
     /**
@@ -37,6 +40,36 @@ public class WikiPhilosophy {
      * @throws IOException
      */
     public static void testConjecture(String destination, String source, int limit) throws IOException {
-        // TODO: FILL THIS IN!
+        String url = source;
+        for (int i = 0; i < limit; i++) {
+            if (visited.contains(url)) {
+                System.err.println("같은 링크를 돌고 있습니다.");
+                return;
+            } else {
+                visited.add(url);
+            }
+
+            Element element = getFirstValidLink(url);
+            if (element == null) {
+                System.err.println("유효한 링크가 없습니다");
+                return;
+            }
+
+            System.out.println("**" + element.text() + "**");
+            url = element.absUrl("href");
+
+            if (url.equals(destination)) {
+                System.out.println("찾았습니다.");
+                break;
+            }
+        }
+    }
+
+    public static Element getFirstValidLink(String url) throws IOException {
+        System.out.println("접속중 : " + url);
+        Elements paragraphs = wf.fetchWikipedia(url);
+        WikiParser wp = new WikiParser(paragraphs);
+        Element element = wp.findFirstLink();
+        return element;
     }
 }
