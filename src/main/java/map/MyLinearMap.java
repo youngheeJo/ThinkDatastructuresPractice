c6f8b8737a3466a20c68c7e9fe352b51a4e0ecc1
youngheeJo <younghee.dev@gmail.com>
2022-04-19 10:17:47 +0900

    Chapter9. MyLinearMap 메서드 구현

--- src/main/java/map/MyLinearMap.java
+++ src/main/java/map/MyLinearMap.java
@@ -1,14 +1,9 @@
 /**
  *
  */
-package com.allendowney.thinkdast;
+package map;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Implementation of a Map using a List of entries, so most
@@ -63,7 +58,11 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 	 * @param target
 	 */
 	private Entry findEntry(Object target) {
-		// TODO: FILL THIS IN!
+		for (Entry entry : entries) {
+			if (equals(target, entry.getKey())) {
+				return entry;
+			}
+		}
 		return null;
 	}
 
@@ -98,8 +97,8 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V get(Object key) {
-		// TODO: FILL THIS IN!
-		return null;
+		Entry entry = findEntry(key);
+		return entry == null ? null : entry.getValue();
 	}
 
 	@Override
@@ -118,8 +117,15 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V put(K key, V value) {
-		// TODO: FILL THIS IN!
-		return null;
+		Entry entry = findEntry(key);
+		if (entry == null) {
+			entries.add(new Entry(key, value));
+			return null;
+		} else {
+			V oldValue = entry.getValue();
+			entry.setValue(value);
+			return oldValue;
+		}
 	}
 
 	@Override
@@ -131,8 +137,13 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 
 	@Override
 	public V remove(Object key) {
-		// TODO: FILL THIS IN!
-		return null;
+		Entry entry = findEntry(key);
+		if (entry == null) {
+			return null;
+		} else {
+			entries.remove(entry);
+			return entry.getValue();
+		}
 	}
 
 	@Override
@@ -172,7 +183,7 @@ public class MyLinearMap<K, V> implements Map<K, V> {
 	 *
 	 * @return
 	 */
-	protected Collection<? extends java.util.Map.Entry<K, V>> getEntries() {
+	protected Collection<? extends Map.Entry<K, V>> getEntries() {
 		return entries;
 	}
 }
